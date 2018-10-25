b81fa0d0d2e19acbc79c87456821bd91732671cc
aboud20-meet <aboud20@meet.mit.edu>
2018-10-25 18:49:15 +0300

    duhfuid

--- app.py
+++ app.py
@@ -3,7 +3,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return "You are home!"
+    return render_template("home.html")
+
+@app.route('/shop')
+def home():
+    return render_template("shop.html")
+
+@app.route('/LIST')
+def LIST():
+    pets = ["EYELASH GLUE", "EYELEASHES", "TAMARA"] 
+    return render_template(
+"LIST.html",
+pets=pets)
+
+    
 
 if __name__ == '__main__':
    app.run(debug = True)
